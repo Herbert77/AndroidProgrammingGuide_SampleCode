88f80624311b9ee6f9258c39dc1232942fe53c66
HerbertHu <herbert7789@hotmail.com>
2022-11-07 17:48:07 +0800

    Chapter 2 源码示例提交。
    知识要点：
    
    1、修饰class 的 data 关键字；
    2、使用 tools命名空间，tools:text；
    3、把图片资源引入到 res 目录的drawable 文件夹；
    4、设置控件大小使用到的像素单位（px、dp、sp）；
    5、ImageButton 的使用。

--- 01/app/src/main/java/com/cocoa/geoquiz/MainActivity.kt
+++ 01/app/src/main/java/com/cocoa/geoquiz/MainActivity.kt
@@ -4,12 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var preButton: ImageButton
+    private lateinit var nextButton: ImageButton
+    private lateinit var questionTextView: TextView
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_asia, true),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_mideast, false))
+
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,18 +31,68 @@ class MainActivity : AppCompatActivity() {
 
         trueButton = findViewById<Button>(R.id.true_button)
         falseButton = findViewById<Button>(R.id.false_button)
+        preButton = findViewById(R.id.previous_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
 
         // 添加控件监听器
         trueButton.setOnClickListener {
-            var toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)
-            toast.setGravity(Gravity.TOP, 0, 0)
-            toast.show()
+            checkAnswer(true)
         }
 
         falseButton.setOnClickListener {
-            var toast = Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT)
-            toast.setGravity(Gravity.BOTTOM, 0, 0)
-            toast.show()
+            checkAnswer(false)
         }
+
+        preButton.setOnClickListener {
+            if (currentIndex > 0) {
+                currentIndex -= 1
+            }
+            else {
+                currentIndex = questionBank.size - 1
+            }
+            updateQuestion()
+        }
+
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        questionTextView.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        updateQuestion()
+    }
+
+    // 更新问题
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+
+    // 根据用户输入答案和标准答案进行对比, 显示结果提示
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+
+        val toastMsgResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        }
+        else {
+            R.string.incorrect_toast
+        }
+
+        var toast = Toast.makeText(this, toastMsgResId, Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.BOTTOM, 0, 0)
+        toast.show()
     }
 }
+
+/**
+ *  关于图片大小的设置,有几个大小的单位
+ *  px : short for pixel.
+ *  dp : short for density-independent(与密度无关的像素) pixel. One dp is always 1/160 of an inch on a device’s screen.
+ *  sp : short for scale-independent(与缩放无关的像素) pixel. 关联到了用户偏好的字体大小.设置字体使用 sp.
+ */
---
+++ 01/app/src/main/java/com/cocoa/geoquiz/Question.kt
@@ -0,0 +1,8 @@
+package com.cocoa.geoquiz
+
+import androidx.annotation.StringRes
+
+// data 关键字作用: 自动实现类的 equals(), hashCode(), toString()
+data class Question(@StringRes val textResId: Int, val answer: Boolean)
+
+
