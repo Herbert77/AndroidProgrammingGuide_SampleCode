16dd1e024e1f7c39d5bae841d40d5b07c01992f1
HerbertHu <herbert7789@hotmail.com>
2022-11-07 15:50:56 +0800

    Chapter 1 源码示例提交。
    知识要点：
    
    1、编辑xml（代码or拖动控件）；
    2、在activity中引用控件；
    3、给控件添加监听器；
    4、显示Toast。

--- 01/app/src/main/java/com/cocoa/geoquiz/MainActivity.kt
+++ 01/app/src/main/java/com/cocoa/geoquiz/MainActivity.kt
@@ -2,10 +2,33 @@ package com.cocoa.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        trueButton = findViewById<Button>(R.id.true_button)
+        falseButton = findViewById<Button>(R.id.false_button)
+
+        // 添加控件监听器
+        trueButton.setOnClickListener {
+            var toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP, 0, 0)
+            toast.show()
+        }
+
+        falseButton.setOnClickListener {
+            var toast = Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.BOTTOM, 0, 0)
+            toast.show()
+        }
     }
 }
