e34bbcb479834880bc872580f958607e1db964c3
HerbertHu <herbert7789@hotmail.com>
2022-11-09 16:43:48 +0800

    Chapter 4 源码示例提交。
    知识要点：
    
    1、使用 ViewModel 来持有并处理 activity UI页面会使用到的数据；
    
    2、为了使用 ViewModel，需要在 build.gradle (Module: app) 构建文件中，添加 androidx.lifecycle:lifecycle-extensions:2.2.0 依赖项，并且进行 async now，对依赖库文件进行下载；
    
    3、安卓项目有两个构建文件，一个属于项目，一个属于App；
    
    4、ViewModel 在被销毁前，会回调自身的 onCleared()方法；
    
    5、在 activity 中，通过 ViewModelProvider 获取到跟当前 activity 关联的 provider，然后通过该 provider 获取到 viewModel；
    
    6、activity 的 isFinishing 属性，只有当用户点击 back 的时候，才是 true。如果由于配置改变而销毁 activity ，这个 isFinishing 的值仍是 false；
    
    7、viewModel 的生命周期更符合使用者的期待。在activity还未完成时，viewModel 也不会被销毁，哪怕 activity 因为配置改变而重新构建；
    
    8、在使用 activity 和 viewModel 的时候，注意循环引用问题，activity 持有 viewModel ，反过来则不持有；
    
    9、把 activity 页面上的数据和逻辑尽可能的安排到 viewModel 中，让 activity 更单纯的处理页面上的交互事件；
    
    10、当系统终止某个app的进程时，app 中的所有 activity 和 viewModel 都会被一并清理掉；
    
    11、为了解决10所提到的问题，我们可以通过API - Activity.onSaveInstanceState(Bundle) 对页面的数据进行暂存为bundle，被系统写入磁盘中，故数据宜简单，这个数据也被称为 activity record。这个 API 在 activity 未完成的状态下，状态切入到 stopped 的时候，系统都会主动调用；
    
    12、bundle 是一种把字符串作为键，以映射到几个特定类型值的数据结构；
    
    13、在系统中开启or关闭开发者模式，在开发者模式中，开启 Don't Keep Activity 选项；
    
    14、如何混合运用这两种暂存数据的技术呢？比较好的方法是，在 ViewModel 中存大量的复杂的数据，在 bundle 中存储少量的关键性的数据。通过 bundle 中恢复出的关键数据，来配置并激活 ViewModel，使其具备还原视图的能力。

--- 01/app/src/main/java/com/cocoa/geoquiz/MainActivity.kt
+++ 01/app/src/main/java/com/cocoa/geoquiz/MainActivity.kt
@@ -2,13 +2,20 @@ package com.cocoa.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import android.view.Gravity
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import java.security.Key
 
 private const val TAG = "MainActivity"
+private const val KEY_INDEX = "index"
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,20 +25,25 @@ class MainActivity : AppCompatActivity() {
     private lateinit var nextButton: ImageButton
     private lateinit var questionTextView: TextView
 
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_asia, true),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_mideast, false))
-
-    private var currentIndex = 0
+    // 把viewModel声明为懒加载属性
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProvider(this).get(QuizViewModel::class.java)
+    }
 
     // activity的状态方法
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        android.util.Log.d(TAG, "onCreate: called")
+        Log.d(TAG, "onCreate: called")
         setContentView(R.layout.activity_main)
+
+        // 从 savedInstanceState 中取出可能存在的 bundle 数据
+        val currentIndex = savedInstanceState?.getInt(KEY_INDEX) ?: 0
+        quizViewModel.currentIndex = currentIndex
+
+        // 把 viewModel 和当前 Activity 关联起来
+//        val provider: ViewModelProvider = ViewModelProvider(this)
+//        val quizViewModel = provider.get(QuizViewModel::class.java)
+//        Log.d(TAG, "Got a QuizViewModel: $quizViewModel.")
 
         trueButton = findViewById<Button>(R.id.true_button)
         falseButton = findViewById<Button>(R.id.false_button)
@@ -49,22 +61,17 @@ class MainActivity : AppCompatActivity() {
         }
 
         preButton.setOnClickListener {
-            if (currentIndex > 0) {
-                currentIndex -= 1
-            }
-            else {
-                currentIndex = questionBank.size - 1
-            }
+            quizViewModel.moveToPrev()
             updateQuestion()
         }
 
         nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
             updateQuestion()
         }
 
         questionTextView.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
             updateQuestion()
         }
 
@@ -86,6 +93,13 @@ class MainActivity : AppCompatActivity() {
         android.util.Log.d(TAG, "onPause: called")
     }
 
+    // 覆写该方法,当该 activity 进入 Stoped 状态时, 该方法会被调用, 把 活动的 数据保存在 bundle 中, 如果该 activity 被重新创建时, 会通过 onCreate() 方法,恢复保存的数据.
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.i(TAG, "onSaveInstanceState: ")
+        outState.putInt(KEY_INDEX, quizViewModel.currentIndex)
+    }
+
     override fun onStop() {
         super.onStop()
         android.util.Log.d(TAG, "onStop: called")
@@ -98,13 +112,13 @@ class MainActivity : AppCompatActivity() {
 
     // 更新问题
     private fun updateQuestion() {
-        val questionTextResId = questionBank[currentIndex].textResId
+        val questionTextResId = quizViewModel.currentQuestionText
         questionTextView.setText(questionTextResId)
     }
 
     // 根据用户输入答案和标准答案进行对比, 显示结果提示
     private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = questionBank[currentIndex].answer
+        val correctAnswer = quizViewModel.currentQuestionAnswer
 
         val toastMsgResId = if (userAnswer == correctAnswer) {
             R.string.correct_toast
---
+++ 01/app/src/main/java/com/cocoa/geoquiz/QuizViewModel.kt
@@ -0,0 +1,47 @@
+package com.cocoa.geoquiz
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+private const val TAG = "QuizViewModel"
+
+class QuizViewModel: ViewModel() {
+
+//    init {
+//        Log.d(TAG, "ViewModel instance created")
+//    }
+//
+//    override fun onCleared() {
+//        super.onCleared()
+//        Log.d(TAG, "onCleared: ViewModel instance about to be destroyed.")
+//    }
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_asia, true),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_mideast, false))
+
+    var currentIndex = 0
+
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].answer
+
+    val currentQuestionText: Int
+        get() = questionBank[currentIndex].textResId
+
+    fun moveToNext() {
+        currentIndex = (currentIndex + 1) % questionBank.size
+    }
+
+    fun moveToPrev() {
+        currentIndex = if (currentIndex > 0) {
+            currentIndex - 1
+        }
+        else {
+            questionBank.size - 1
+        }
+    }
+
+}
